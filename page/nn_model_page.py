1c803035f87b3f9a03ca9a5a542fa04913c11b7e
Boom-Tris <boomtris.jontama@gmail.com>
2025-03-03 00:31:52 +0700

    Update nn_model_page.py

--- page/nn_model_page.py
+++ page/nn_model_page.py
@@ -52,7 +52,7 @@ def download_youtube_audio(url):
                 'preferredquality': '192',
             }],
             'headers': {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
             }
         }
 
